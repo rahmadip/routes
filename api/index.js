06a31c528914fcf0ec67ed2ed47a1809412bfa66
rahmadip <frahmadip@gmail.com>
2025-12-25 21:26:56 +0700

    version 1.0

--- api/index.js
+++ api/index.js
@@ -5,7 +5,9 @@ const { createClient } = require('@supabase/supabase-js');
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+    cors(origin = 'https://rahmadip.github.io')
+);
 
 const url = process.env.URL;
 const key = process.env.KEY;
@@ -35,9 +37,25 @@ app.get('/tools', async (req, res) => {
     try {
         const { data, error, status } = await supabase
             .from('svg')
-            .select('*')
+            .select('section, name, path, viewbox')
             .eq('display', true)
             .order('id', { ascending: true });
+        if (error) {
+            throw { error, status };
+        }
+        return res.json(data);
+    } catch ({ error, status }) {
+        return res.status(status).json({ status, error: error.message });
+    }
+});
+
+app.get('/tools/:tool', async (req, res) => {
+    const { tool } = req.params;
+    try {
+        const { data, error, status } = await supabase
+            .from('svg')
+            .select('name, path, viewbox')
+            .in('name', tool.split(','));
         if (error) {
             throw { error, status };
         }
@@ -51,13 +69,83 @@ app.get('/space', async (req, res) => {
     try {
         const { data, error, status } = await supabase
             .from('space')
-            .select('*')
+            .select('path, viewbox')
             .eq('display', true)
             .order('id', { ascending: true });
         if (error) {
             throw { error, status };
         }
         return res.json(data);
+    } catch ({ error, status }) {
+        return res.status(status).json({ status, error: error.message });
+    }
+});
+
+app.get('/projects', async (req, res) => {
+    try {
+        const { data, error, status } = await supabase
+            .from('projects')
+            .select('path, title, type, tools, tags, thumbnail')
+            .eq('display', true)
+            .order('id', { ascending: false });
+        if (error) {
+            throw { error, status };
+        }
+        return res.json(data);
+    } catch ({ error, status }) {
+        return res.status(status).json({ status, error: error.message });
+    }
+});
+
+app.get('/projects/:path', async (req, res) => {
+    const { path } = req.params;
+    try {
+        const { data, error, status } = await supabase
+            .from('projects')
+            .select('title, type, desc, tags, links, tools, contents, basis')
+            .eq('path', path)
+            .eq('display', true)
+            .single();
+        if (error) {
+            throw { error, status };
+        }
+        return res.json(data);
+    } catch ({ error, status }) {
+        return res.status(status).json({ status, error: error.message });
+    }
+});
+
+app.get('/projects/:path/neq', async (req, res) => {
+    const { path } = req.params;
+    try {
+        const { data, error, status } = await supabase
+            .from('projects')
+            .select('path, title, type, thumbnail')
+            .eq('display', true)
+            .neq('path', path)
+            .limit(5);
+        if (error) {
+            throw { error, status };
+        }
+        return res.json(data);
+    } catch ({ error, status }) {
+        return res.status(status).json({ status, error: error.message });
+    }
+});
+
+app.post('/message', async (req, res) => {
+    const { message, sender } = req.body;
+    try {
+        const { error, status, statusText } = await supabase
+            .from('message')
+            .insert({
+                message: message,
+                sender: sender
+            });
+        if (error) {
+            throw { error, status };
+        }
+        return res.status(status).send(statusText);
     } catch ({ error, status }) {
         return res.status(status).json({ status, error: error.message });
     }
